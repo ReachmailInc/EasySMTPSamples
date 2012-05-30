9b136fb133a39ea35958ba93d09eb5884321741b
Dan Nielsen <dnielsen@sentinelhill.net>
2012-05-30 21:50:05 +0000

    Updated perl script

--- perl/easy_smtp_example.pl
+++ perl/easy_smtp_example.pl
@@ -10,19 +10,20 @@ use strict;
 use warnings;
 use Net::SMTP::TLS;
 
-if (@ARGV < 5) {
-	print "easy_smtp_example.pl <user> <password> <to email> <from email>";
+if (@ARGV < 4) {
+	print "easy_smtp_example.pl <user> <password> <to email> <from email>\n";
 	exit();
 }
 
-my $mailer = new NEW::SMTP::TLS(
+my $mailer = new Net::SMTP::TLS(
 	'ssrs.reachmail.net',
 	Hello => 'ssrs.reachmail.net',
 	Port => 25,
-	User => $ARGV[1],
-	Password => $ARGV[2]);
-$mailer->mail($ARGV[4]);
-$mailer->to($ARGV[3]);
+	User => $ARGV[0],
+	Password => $ARGV[1],
+	Debug => 1);
+$mailer->mail($ARGV[3]);
+$mailer->to($ARGV[2]);
 $mailer->data;
 $mailer->datasend("Subject: EasySMTP test\r\n\r\nTest message");
 $mailer->dataend;
